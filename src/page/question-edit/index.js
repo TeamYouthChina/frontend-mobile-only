ed5c707b4cbf7578f4ef1fd81015f0217fa5d6b1
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2019-04-02 16:46:35 -0400

    add question

--- src/page/question-edit/index.js
+++ src/page/question-edit/index.js
@@ -7,6 +7,8 @@ import {languageHelper} from '../../tool/language-helper';
 import {removeUrlSlashSuffix} from '../../tool/remove-url-slash-suffix';
 import {setToken} from '../../tool/set-token';
 
+import classes from './index.module.css';
+
 export class QuestionEdit extends React.Component {
   constructor(props) {
     super(props);
@@ -28,15 +30,34 @@ export class QuestionEdit extends React.Component {
     }
     // render
     return (
-      <div>
+      <div style={{backgroundColor:'#F0F3FA',height:'100%'}}>
         <div
-          className="cell-wall"
+          className={`cell-wall ${classes.wrapper}`}
         >
-          <div
-            className="cell-membrane"
-          >
-            <span>question-edit</span>
+          <div className={classes.title}>
+            <span className={classes.cancel}>取消</span>
+            <span className={classes.titleAsk}>提问题</span>
+            <span className={classes.addTitle}>发布</span>
           </div>
+        </div>
+        <div className="cell-wall">
+          <ul className={classes.content}>
+            <li className={classes.titleDes}>问题标题</li>
+            <li>
+              <input className={classes.inputStyle} type="text" placeholder="简述你的问题"/>
+            </li>
+            <li>
+              <textarea className={classes.textStyle} type="text" placeholder="为你的问题添加更详细的描述，让职场达人更好的帮助你哦"/>
+            </li>
+            <div className={classes.tags}>
+              <span className={classes.tagSpan}>公司福利</span>
+              <span className={classes.tagSpan}>面试技巧</span>
+              <span className={classes.tagSpan}>合理陶瓷</span>
+              <span className={classes.addTag}>
+                +新标签
+              </span>
+            </div>
+          </ul>
         </div>
       </div>
     );
